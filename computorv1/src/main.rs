1dcb84db351b90bbdba221b6f58796009f6cabb6
Clara COTTIN <ccottin@made-f0Dr7s1.clusters.42paris.fr>
2024-10-14 19:27:32 +0200

    theres a quote issue when passing arguments inside quotes to cargo run

--- computorv1/src/main.rs
+++ computorv1/src/main.rs
@@ -1,33 +1,57 @@
 use std::env;
 
 struct Term {
-    coefficient: i32,
-    exposant: i32,
-    operator: [char; 2],
+    coefficient: f64,
+    exposant: i32, //if 0 == coeff * 1
+    //operator: char,
+    sign: char,
+
 }
 
 fn error(reason: &str) {
     println!("Error: {reason}");
 }
 
-fn check_polynomial(polynomial: &String) {
-    return;
-}
+fn is_valid_char(arg: &String) -> bool {
+    let valid_chars: [u8; 7] = [b'.', b'+', b'-', b'*', b'=', b'x', b'^'];
 
-fn args_checker(args: &Vec::<String>) -> String {
-    let mut equal = 0;
-
-    for s in args {
-
-        println!("{s}");
-        let byte = s.as_bytes();
-
-        for (i, &item) in byte.iter().skip(1).enumerate() {
-            println!("for {i} arg is {item}");
+    for b in arg.as_bytes() {
+        if !valid_chars.contains(&b) && !b.is_ascii_digit() {
+            return false
         }
     }
-    "".to_string()
+    true
+    // We just did this and theres a quote issue
 }
+
+fn args_checker(args: &[String]) -> (i32, String) {
+    
+    let mut equal = 0;
+    let mut arg = String::new();
+
+    for s in args {
+        arg = arg + s
+    }
+    if !arg.is_ascii() || !is_valid_char(&arg) {
+        return (1, "Unvalid set of caracters".to_string())
+    }
+
+    println!("{arg}");
+    let c = arg.as_bytes();
+
+    for (i, &item) in c.iter().enumerate() {
+        println!("for {i} arg is {item}");
+        if item == b'=' {
+            equal += 1;
+        }
+
+    }
+
+    println!("{equal}");
+    (0, "".to_string())
+}
+
+// *------------------------------* //
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -35,23 +59,23 @@ fn main() {
         error("Not enough arguments.");
         return;
     }
-    if args_checker(&args).is_empty()  {
-        error("Unvalid format");
-      //  return;
+
+    // let test = Term {
+    //     coefficient : 2,
+    //     exposant : 1,
+    //     sign : '-',
+    // };
+  //  println!("{0}, {1}, {2}", test.coefficient, test.exposant, test.sign[1]);
+
+    let arg = args_checker(&args[1..]);
+    if arg.0 != 0 {
+        error(&arg.1);
     }
 
-    let test = Term {
-        coefficient : 2,
-        exposant : 1,
-        operator : ['-', '+'],
-    };
-
-    println!("{0}, {1}, {2}", test.coefficient, test.exposant, test.operator[1]);
     //potentially add a checker here
     //Note that std::env::args will panic if any argument contains
     //invalid Unicode.
 
    // check_polynomial(&polynomial);
 
-    dbg!(args);
 }
