2f20738bf414929a159e6833cd37123016f796ee
ccottin <cottin.clara7@gmail.com>
2024-10-18 19:18:14 +0200

    sorting char from str input, using an useless match for the sake of learning

--- computorv1/src/main.rs
+++ computorv1/src/main.rs
@@ -24,13 +24,13 @@ fn is_valid_char(arg: &String) -> bool {
     true
 }
 
-fn args_checker(args: [String]) -> (i32, String) {
-    
+fn args_checker(args: &[String]) -> (i32, String) {
+
     let mut equal = 0;
     let mut arg = String::new();
 
     for s in args {
-        arg = arg + s
+        arg = arg + s;
     }
     if !arg.is_ascii() || !is_valid_char(&arg) {
         return (1, "Unvalid set of caracters".to_string())
@@ -38,27 +38,103 @@ fn args_checker(args: [String]) -> (i32, String) {
 
     let c = arg.as_bytes();
 
-    for (i, &item) in c.iter().enumerate() {
+    for (_i, &item) in c.iter().enumerate() {
         if item == b'=' {
             equal += 1;
         }
     }
-    if (equal != 1) {
-        return (1, "Unvalid equality sign")
+    if equal != 1 {
+        return (1, "Unvalid equality sign".to_string())
     }
     (0, arg)
 }
 
-fn  parser(arg :String) -> (vec<Term>, vec<Term>) {
+fn  is_sign(c: char) -> bool {
+    if c == '+' || c == '-' || c == '*' || c == '/' {
+        return true
+    }
+    false
+}
+#[derive(Debug)]
+enum Possibilities {
+    Digit(char),                   // ("0123456789")
+    Signs(char),                   // ("+-*/")
+    WhiteSpace(char),              // (' ')
+    Unknown(char),                 // ('x')
+    Power(char),                   // ('^')
+    Equal(char),                   // ('=')
+    Dot(char),                     // ('.')
+}
+
+fn  check_possibilities(checked: Possibilities, base: &String, index: usize, term: &Term)
+    -> (isize, &Term) {
+    match checked {
+        Possibilities::Digit(c) => {
+            println!("{:?}", checked);
+
+        }
+        Possibilities::Signs(c) => {
+
+        }
+        Possibilities::WhiteSpace(c) => {
+
+        }
+        Possibilities::Unknown(c) => {
+
+        }
+        Possibilities::Power(c) => {
+
+        }
+        Possibilities::Equal(c) => {
+
+        }
+        Possibilities::Dot(c) => {
+
+        }
+    }
+}
+
+fn  parser(arg: String) -> (Vec<Term>, Vec<Term>) {
+
+
     let mut left_expression: Vec<Term> = Vec::new();
     let mut right_expression: Vec<Term> = Vec::new();
 
-    let c = arg.as_bytes();
 
-    for (i, &item) in c.iter().enumerate() {
+    for (i, item) in arg.char_indices() {
         
+        if is_sign(item) {
+            println!("sign");
+            check_possibilities(Possibilities::Signs(item), &arg, i);
+        }
+        else if item.is_numeric() {
+            println!("digit");
+            check_possibilities(Possibilities::Digit(item), &arg, i);
+        }
+        else if item == ' ' {
+            println!("espasse");
+            check_possibilities(Possibilities::WhiteSpace(item), &arg, i);
+        }
+        else if item == 'x' {
+            println!("x");
+            check_possibilities(Possibilities::Unknown(item), &arg, i);
+        }
+        else if item == '=' {
+            println!("=");
+            check_possibilities(Possibilities::Equal(item), &arg, i);
+        }
+        else if item == '.' {
+            println!(".");
+            check_possibilities(Possibilities::Dot(item), &arg, i);
+        }
+        else if item == '^' {
+            println!("^");
+            check_possibilities(Possibilities::Power(item), &arg, i);
+        }
+        else {
+        println!("WTF");
+        }
     }
-  //might use a match here 
     (left_expression, right_expression)
 }
 
@@ -78,10 +154,11 @@ fn main() {
     // };
   //  println!("{0}, {1}, {2}", test.coefficient, test.exposant, test.sign[1]);
 
-    let arg = args_checker(args[1..]);
+    let arg = args_checker(&args[1..]);
     if arg.0 != 0 {
         error(&arg.1);
     }
+    parser(arg.1);
 
 
 
