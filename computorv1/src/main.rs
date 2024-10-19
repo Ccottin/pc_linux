0ce5cf1ffd9a776ef032910a95cf878ba3a4bc6f
ccottin <cottin.clara7@gmail.com>
2024-10-19 15:34:03 +0200

    match understood, lets get rid of this boilerplate code and move to a more understanable, pretty one. Push before removing match from parser function

--- computorv1/src/main.rs
+++ computorv1/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+#[derive(Debug)]
 struct Term {
     coefficient: Option<f64>,
     exposant: Option<i32>, //if 0 == coeff * 1
@@ -73,6 +74,14 @@ fn  is_sign(c: char) -> bool {
     false
 }
 
+fn  is_numeric(c: u8) -> bool {
+    if c == b'0' || c == b'1' || c == b'2' || c == b'3' || c == b'4'
+        || c == b'5' || c == b'6' || c == b'7' || c == b'8' || c == b'9' {
+        return true
+    }
+    false
+}
+
 #[derive(Debug)]
 enum Possibilities {
     Digit(char),                   // ("0123456789")
@@ -84,29 +93,71 @@ enum Possibilities {
     Dot(char),                     // ('.')
 }
 
-fn  check_possibilities(checked: Possibilities, base: &String, index: usize, term: &mut Term)
+fn  check_possibilities(checked: Possibilities, base: &[u8], index: &mut usize, term: &mut Term, nb_elem: isize)
     -> isize {
     match checked {
         Possibilities::Digit(c) => {
-            println!("{:?}", checked);
+            match term.coefficient {
+                Some(i) => {
+                    if base[index - 1] == b' ' && term.coefficient != None {
+                        return -1
+                    }
+                    else if base[index - 1] == b'.' {
+                        term.coefficient = Some(i + c.to_digit(10).unwrap() as f64 / 10.0);
+                    }  
+                    else {
+                        term.coefficient = Some(i * 10.0);
+                        term.coefficient = Some(i * c.to_digit(10).unwrap() as f64);
+                    }
+                }
+                None => term.coefficient = Some(c.to_digit(10).unwrap() as f64),
+            }
+            println!("{:?}", term.coefficient);
             0
         }
         Possibilities::Signs(c) => {
+
             0
         }
-        Possibilities::WhiteSpace(c) => {
-            0
-        }
+        Possibilities::WhiteSpace(c) => 0,
         Possibilities::Unknown(c) => {
-            0
+            let search = index - 1;
+            while search >= 0 && base[search] == b' ' {
+                search -= 1;
+            }
+            if is_sign(base[search] as char) || search == 0 || base[search] == '=' {
+                return 0
+            -1
+            }
         }
         Possibilities::Power(c) => {
-            0
+            let search = index - 1;
+            while search >= 0 && base[search] != b'x' && base[search] == b' ' {
+                search -= 1;
+            }
+            if base[search] == b'x' {
+                return 0
+            }
+            search = index + 1;
+            while search <= base.len() && base[search] == b' ' {
+                search += 1;
+            }
+            if !is_sign(char(base[search + 1])) || base[search + 1] != b' ' {
+                return -1
+            }
+            if base[search] != b'0' || base[search] != b'1' || base[search] == b'2' {
+                term.exposant = Some((base[search]).to_digit(10).unwrap as int32);
+                *index = search + 1;
+                return 0
+            }
+            -1
         }
-        Possibilities::Equal(c) => {
-            0
-        }
+        Possibilities::Equal(c) => 2,
         Possibilities::Dot(c) => {
+            if !(is_numeric(base[index - 1])
+                && is_numeric(base[index + 1])) {
+                    return -1
+                }
             0
         }
     }
@@ -121,40 +172,45 @@ fn  parser(arg: String) -> (Vec<Term>, Vec<Term>) {
     let mut right_expression: Vec<Term> = Vec::new();
     let mut to_fill = Term::new();
     let mut ret = 0;
+    let mut nb_elem = 0;
     
-    for (i, item) in arg.char_indices() {
+    for (mut i, item) in arg.char_indices() {
         
-        ret = if is_sign(item) {
+        if is_sign(item) {
             println!("sign");
-            check_possibilities(Possibilities::Signs(item), &arg, i, &mut to_fill)
+            ret = check_possibilities(Possibilities::Signs(item), arg.as_bytes(), &mut i, &mut to_fill, nb_elem)
         }
-        ret = else if item.is_numeric() {
+        else if item.is_numeric() {
             println!("digit");
-            check_possibilities(Possibilities::Digit(item), &arg, i, &mut to_fill)
+            ret = check_possibilities(Possibilities::Digit(item), arg.as_bytes(), &mut i, &mut to_fill, nb_elem)
         }
-        ret = else if item == ' ' {
+        else if item == ' ' {
             println!("espasse");
-            check_possibilities(Possibilities::WhiteSpace(item), &arg, i, &mut to_fill)
+            ret = check_possibilities(Possibilities::WhiteSpace(item), arg.as_bytes(), &mut i, &mut to_fill, nb_elem)
         }
-        ret = else if item == 'x' {
+        else if item == 'x' {
             println!("x");
-            check_possibilities(Possibilities::Unknown(item), &arg, i, &mut to_fill)
+            ret = check_possibilities(Possibilities::Unknown(item), arg.as_bytes(), &mut i, &mut to_fill, nb_elem)
         }
-        ret = else if item == '=' {
+        else if item == '=' {
             println!("=");
-            check_possibilities(Possibilities::Equal(item), &arg, i, &mut to_fill)
+            ret = check_possibilities(Possibilities::Equal(item), arg.as_bytes(), &mut i, &mut to_fill, nb_elem)
         }
-        ret = else if item == '.' {
+        else if item == '.' {
             println!(".");
-            check_possibilities(Possibilities::Dot(item), &arg, i, &mut to_fill)
+            ret = check_possibilities(Possibilities::Dot(item), arg.as_bytes(), &mut i, &mut to_fill, nb_elem)
         }
-        ret = else if item == '^' {
+        else if item == '^' {
             println!("^");
-            check_possibilities(Possibilities::Power(item), &arg, i, &mut to_fill)
+            ret = check_possibilities(Possibilities::Power(item), arg.as_bytes(), &mut i, &mut to_fill, nb_elem)
         }
-        ret = else {
+        else {
         println!("WTF");
-        -1
+        ret = -1;
+        }
+        println!("after loop {}, truct = {:?}", i, to_fill);
+        if ret == -1 {
+            println!("error");
         }
         //evaluer si ret = -1 ou 1
     }
@@ -180,6 +236,7 @@ fn main() {
     let arg = args_checker(&args[1..]);
     if arg.0 != 0 {
         error(&arg.1);
+        return;
     }
     parser(arg.1);
 
